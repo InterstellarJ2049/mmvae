85541bb126d6fc5aed55db4f5e57f0d1cb641a8a
Yijie <yijie-zhang@uiowa.edu>
2025-05-28 23:26:18 -0500

    05-28_3_gpu3_cubIS_iwae_K1_B128_D128_s2_e200

--- src/commands/run_original_MMVAE_CUB_experiment.sh
+++ src/commands/run_original_MMVAE_CUB_experiment.sh
@@ -18,7 +18,7 @@ fi
 # TODO (Yijie): add elboVCCA and dregVCCA replacing vcca
 MODEL="cubIS"  # mnist_svhn, cubISft, cubIS
 OBJ="iwae"  # elbo, dreg, iwae
-BATCH=256  # new_release: 32
+BATCH=128  # new_release: 32
 K=1  # new_release: 10
 EPOCHS=200  # new_release: 50
 SEED=2  # new_release: 2
@@ -33,8 +33,8 @@ SHARED_LAT_DIM=128  # new_release: 48
 # MVIB_L_SC=0.0
 
 date=`echo $(date '+%m-%d')`
-number=2
-gpuid=2
+number=3
+gpuid=3
 note=""
 
 # new_release: dreg+Z48+W16+K10+Normal
